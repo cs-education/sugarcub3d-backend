2ad28402e45d6d569a9740df2593ad4452105a68
Sukeerthi R Khadri <khadri2@illinois.edu>
2016-03-05 16:02:49 -0600

    GET \questions working. POST \questions working with prevention of duplicate question creation.

--- routes/api.js
+++ routes/api.js
@@ -15,6 +15,7 @@ var Question = require('../models/questions');
 var requestCount = 0;
 router.use(function (req, res, next) {
 	console.log('Time of request : ', Date.now());
+	requestCount++;
 	console.log('Request count : ', requestCount);
 	next();
 });
@@ -64,7 +65,7 @@ questionsRoute.get(function (req, res) {
 	findQuery.exec(function (err, questions) {
 		if (err) {
 			console.error(err.stack);
-			res.status(500).json({ message: 'HTTP 500 - Internal Server Error. A team of CS majors are on the case!'});
+			res.status(500).json({ message: 'HTTP 500 - Internal Server Error.'});
 		}
 		else {
 			// Success
@@ -78,17 +79,81 @@ POST - Create or update an existing question
 */
 questionsRoute.post(function (req, res) {
 	// find question in database before creationg
-	/*if (req.params.questionText) {
-		Question.findOne({'questionText' : req.params.questionText}, function (err, question) {
+		Question.findOne({'questionText' : req.body.questionText}, function (err, question) {
 
 			if (err) {
 				console.error(err.stack);
+				res.status(500).json({message: 'HTTP 500 - Internal Server Error'});
 			}
-			else { // question already exists
+			else {	// no error, check if question exists
+
+				if (!question) {	// question doesn't exist, so create new one	
+					
+					// check if required params are provided
+					if (!req.body.questionType || !req.body.questionText || !req.body.hintText || !req.body.correctAnswerText || !req.body.optionsText || !req.body.explanationText || !req.body.optionsText.A || !req.body.optionsText.B || !req.body.optionsText.C || !req.body.optionsText.D) {
+
+						// req params not provided
+						res.status(400).json({message: 'HTTP 400 - Bad Request. All required parameters for object creation not provided.'});
+						console.error('HTTP 400 - Bad Request. All required parameters for object creation not provided.');
+					}
+					else {	// all required parameters provided
+						var newQues = new Question();
+
+						newQues.questionType = req.body.questionType;
+						newQues.questionText = req.body.questionText;
+						newQues.hintText = req.body.hintText;
+						newQues.correctAnswerText = req.body.correctAnswerText;
+						newQues.optionsText = req.body.optionsText;
+						newQues.explanationText = req.body.explanationText;
+
+						// save question
+						newQues.save(function (err, savedQuestion) {
+							if (err) {
+								console.error(err.stack);
+								res.status(500).json({message: 'HTTP 500 - Internal Server Error'});
+							}
+							else {
+								// question saved
+								res.status(201).json({message: 'Question saved', data: savedQuestion});
+							}
+						});
+
+					}
+				}
+				else {	// question exists, update its values
+
+					// check if required params are provided
+					if (!req.body.questionType || !req.body.questionText || !req.body.hintText || !req.body.correctAnswerText || !req.body.optionsText || !req.body.explanationText || !req.body.optionsText.A || !req.body.optionsText.B || !req.body.optionsText.C || !req.body.optionsText.D) {
+
+						// req params not provided
+						res.status(400).json({message: 'HTTP 400 - Bad Request. All required parameters for object creation not provided.'});
+						console.error('HTTP 400 - Bad Request. All required parameters for object creation not provided.');
+					}
+					else {	// params provided, update question
+						question.questionType = req.body.questionType;
+						question.questionText = req.body.questionText;
+						question.hintText = req.body.hintText;
+						question.correctAnswerText = req.body.correctAnswerText;
+						question.optionsText = req.body.optionsText;
+						question.explanationText = req.body.explanationText;
+						// update date created with date last modified
+						question.dateCreated = Date.now();
+
+						// save/update question
+						question.save(function (err, savedQuestion) {
+							if (err) {
+								console.error(err.stack);
+								res.status(500).json({message: 'HTTP 500 - Internal Server Error'});
+							}
+							// question saved
+							res.status(200).json({message: 'Question updated', data: savedQuestion});
+						});
+					}
+				}
 
 			}
-		})
-	} */
+		});
+	 
 });
 
 module.exports = router;
