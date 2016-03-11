662c6321a4011a04220fcce0f380ea85b17e9887
Sukeerthi R Khadri <khadri2@illinois.edu>
2016-03-10 23:26:43 -0600

    Added CORS.

--- app.js
+++ app.js
@@ -42,6 +42,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public'))); 
 
+// CORS
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // define base URL for routes
 app.use('/api/questions', questionRouter);
 app.use('/api/users', userRouter);
